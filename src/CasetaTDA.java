bd8df8d46369a7ffcf3138c6c293fff286a76ab5
Alejandro L <A01173657@itesm.mx>
2018-11-04 19:55:50 -0600

    TDA de la caseta

--- src/CasetaTDA.java
+++ src/CasetaTDA.java
@@ -1,3 +1,35 @@
 public class CasetaTDA {
+    private String casetaID;
+    private long entradasT;
+    private long salidasT;
 
+    public CasetaTDA(String casetaID) {
+        this.casetaID = casetaID;
+        entradasT = 0;
+        salidasT = 0;
+    }
+
+    public String getCasetaID() {
+        return casetaID;
+    }
+
+    public void setCasetaID(String casetaID) {
+        this.casetaID = casetaID;
+    }
+
+    public long getEntradasT() {
+        return entradasT;
+    }
+
+    public void setEntradasT(long entradasT) {
+        this.entradasT = entradasT;
+    }
+
+    public long getSalidasT() {
+        return salidasT;
+    }
+
+    public void setSalidasT(long salidasT) {
+        this.salidasT = salidasT;
+    }
 }
