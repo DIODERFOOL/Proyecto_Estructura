733718418840da3131cd45298f8dd87c69391b4a
Alejandro <alejandrolm14@gmail.com>
2018-11-18 10:44:46 -0600

    Modificación de errores

--- src/AutoTDA.java
+++ src/AutoTDA.java
@@ -1,35 +1,3 @@
 public class AutoTDA {
-    private String idAuto;
-    private Boolean entrada;
-    private Boolean salida;
 
-    public AutoTDA(String idAuto, Boolean entrada, Boolean salida) {
-        this.idAuto = idAuto;
-        this.entrada = entrada;
-        this.salida = salida;
-    }
-
-    public String getIdAuto() {
-        return idAuto;
-    }
-
-    public void setIdAuto(String idAuto) {
-        this.idAuto = idAuto;
-    }
-
-    public Boolean getEntrada() {
-        return entrada;
-    }
-
-    public void setEntrada(Boolean entrada) {
-        this.entrada = entrada;
-    }
-
-    public Boolean getSalida() {
-        return salida;
-    }
-
-    public void setSalida(Boolean salida) {
-        this.salida = salida;
-    }
 }
--- src/CarreteraTDA.java
+++
@@ -1,5 +0,0 @@
-public class CarreteraTDA {
-    private CasetaTDA casetas;
-
-
-}
